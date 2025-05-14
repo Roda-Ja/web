2e50079065852950ce7823b02fdb3cd11b7d856c
GunTsy <carlosalexandre3254@gmail.com>
2025-05-14 12:57:37 -0300

    responsible dashboard

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -8,6 +8,7 @@ import {
   BreadcrumbList,
 } from '@/components/ui/breadcrumb'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
 import {
   SidebarInset,
@@ -15,8 +16,15 @@ import {
   SidebarTrigger,
 } from '@/components/ui/sidebar'
 import { File, FileText } from 'lucide-react'
-import { useRef } from 'react'
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts'
+import {
+  Bar,
+  BarChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
 
 const earningsData = [
   { name: '1', Ganhos: 1200 },
@@ -57,12 +65,10 @@ const userOverviewData = [
 ]
 
 export default function Page() {
-  const chartContainerRef = useRef(null)
-
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="flex h-full flex-col overflow-auto">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
@@ -80,7 +86,7 @@ export default function Page() {
               </BreadcrumbList>
             </Breadcrumb>
           </div>
-          <div className="ml-auto flex gap-2 px-4">
+          <div className="ml-auto flex gap-1">
             <Button variant="outline">
               <FileText className="h-4 w-4" /> Excel
             </Button>
@@ -89,133 +95,153 @@ export default function Page() {
             </Button>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="flex gap-4">
-            <div
-              className="grid flex-1 gap-4"
-              style={{
-                gridTemplateAreas: `
-                  "welcome"
-                  "earnings"
-                  "overview"
-                  "users"
-                `,
-                gridTemplateColumns: '1fr',
-                gridTemplateRows: 'auto auto auto auto',
-              }}
-            >
-              <div style={{ gridArea: 'welcome' }}>
-                <h1 className="text-2xl font-bold text-slate-700">
-                  Bem vindo de volta, Raphael!!
-                </h1>
-                <p className="text-sm text-gray-500">
-                  Mar 01, 2025 - Mar 30, 2025
-                </p>
-              </div>
-              <div
-                className="rounded-lg bg-white p-2 shadow"
-                style={{ gridArea: 'earnings' }}
-              >
-                <h2 className="mb-1 text-lg font-bold text-slate-800">
-                  Ganhos de Roda Ja
-                </h2>
-                <div ref={chartContainerRef} className="w-full overflow-hidden">
-                  <BarChart width={1000} height={291} data={earningsData}>
-                    <XAxis dataKey="name" />
-                    <YAxis domain={[0, 4000]} tickCount={11} />
-                    <Tooltip formatter={(value) => `davizende: ${value}`} />
-                    <Legend />
-                    <Bar dataKey="Ganhos" fill="#2B7FFF" />
-                  </BarChart>
-                </div>
-              </div>
-              <div
-                className="rounded-lg bg-white p-2 shadow"
-                style={{ gridArea: 'overview' }}
-              >
-                <h2 className="mb-1 text-lg font-bold text-slate-800">
-                  Overview de Entregas
-                </h2>
-                <div className="flex justify-between">
-                  <div className="text-center">
-                    <p className="text-2xl font-bold">635</p>
-                    <p className="text-sm text-gray-500">DIA</p>
+        <div className="flex-1 overflow-auto p-2 pt-0">
+          <div>
+            <h1 className="text-2xl font-bold text-slate-700">
+              Bem vindo de volta, Raphael!!
+            </h1>
+            <p className="text-sm text-gray-500">Mar 01, 2025 - Mar 30, 2025</p>
+          </div>
+          <div className="mt-4 flex flex-col gap-4 lg:flex-row">
+            <div className="grid min-w-0 flex-1 gap-4">
+              <Card>
+                <CardHeader className="pb-1">
+                  <CardTitle className="text-lg font-bold text-slate-800">
+                    Ganhos de Roda Ja
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="h-[270px] w-full">
+                    <ResponsiveContainer width="100%" height="100%">
+                      <BarChart
+                        data={earningsData}
+                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                      >
+                        <XAxis dataKey="name" />
+                        <YAxis domain={[0, 4000]} tickCount={11} />
+                        <Tooltip formatter={(value) => `R$ ${value}`} />
+                        <Legend />
+                        <Bar
+                          dataKey="Ganhos"
+                          fill="#2B7FFF"
+                          radius={[5, 5, 0, 0]}
+                        />
+                      </BarChart>
+                    </ResponsiveContainer>
                   </div>
-                  <div className="text-center">
-                    <p className="text-2xl font-bold">4.560</p>
-                    <p className="text-sm text-gray-500">SEMANA</p>
-                  </div>
-                  <div className="text-center">
-                    <p className="text-2xl font-bold">20.578</p>
-                    <p className="text-sm text-gray-500">MÊS</p>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="rounded-lg bg-white p-2 shadow"
-                style={{ gridArea: 'users' }}
-              >
-                <h2 className="mb-1 text-lg font-bold text-slate-800">
-                  Overview de Usuários
-                </h2>
-                <div ref={chartContainerRef} className="w-full overflow-hidden">
-                  <BarChart width={1000} height={260} data={userOverviewData}>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="Ativos" fill="#2B7FFF" />
-                    <Bar dataKey="Total" fill="#FE9A00" />
-                  </BarChart>
-                </div>
-              </div>
-            </div>
-            <div className="w-1/3">
-              <div className="mb-4 rounded-lg bg-white p-4 shadow">
-                <h2 className="mb-2 text-lg font-bold text-slate-800">
-                  Motorista Destaque do Mês
-                </h2>
-                <div className="flex flex-col items-center justify-between gap-4">
-                  <img
-                    src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="Antônio Reis"
-                    className="h-24 w-24 rounded-full object-cover"
-                  />
-                  <div className="text-center">
-                    <p className="text-3xl font-bold">Antônio Reis</p>
-                    <div className="flex gap-1">
-                      <p className="text-md flex flex-col p-4">
-                        <span className="text-2xl font-bold">220</span> Corridas
-                      </p>
-                      <p className="text-md flex flex-col border-x border-zinc-200 p-4">
-                        <span className="text-2xl font-bold">R$ 4.840,00</span>
-                        Total de Renda
-                      </p>
-                      <p className="text-md flex flex-col p-4">
-                        <span className="text-2xl font-bold">4.8</span>
-                        Avaliação
-                      </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="pb-1">
+                  <CardTitle className="text-lg font-bold text-slate-800">
+                    Overview de Entregas
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex justify-center gap-6">
+                    <div className="text-center">
+                      <p className="text-4xl font-bold">635</p>
+                      <p className="text-sm text-gray-500">DIA</p>
+                    </div>
+                    <div className="border-r border-l border-gray-300 px-4 text-center">
+                      <p className="text-4xl font-bold">4.560</p>
+                      <p className="text-sm text-gray-500">SEMANA</p>
+                    </div>
+                    <div className="text-center">
+                      <p className="text-4xl font-bold">20.578</p>
+                      <p className="text-sm text-gray-500">MÊS</p>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className="rounded-lg bg-white p-4 shadow">
-                <h2 className="mb-4 text-lg font-bold text-slate-900">
-                  Lista de estabelecimentos ativos
-                </h2>
-                <ul>
-                  {Array(6)
-                    .fill({})
-                    .map((_, index) => (
-                      <li key={index} className="mb-4 flex items-center">
-                        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-blue-500 font-bold text-white">
-                          HP
-                        </div>
-                        <p className="ml-2 text-lg">Hambúrguer Pele</p>
-                      </li>
-                    ))}
-                </ul>
-              </div>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="pb-1">
+                  <CardTitle className="text-lg font-bold text-slate-800">
+                    Overview de Usuários
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="h-[250px] w-full">
+                    <ResponsiveContainer width="100%" height="100%">
+                      <BarChart
+                        data={userOverviewData}
+                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                      >
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar
+                          dataKey="Ativos"
+                          fill="#2B7FFF"
+                          radius={[5, 5, 0, 0]}
+                        />
+                        <Bar
+                          dataKey="Total"
+                          fill="#FE9A00"
+                          radius={[5, 5, 0, 0]}
+                        />
+                      </BarChart>
+                    </ResponsiveContainer>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+            <div className="flex w-full flex-col gap-4 lg:w-[400px]">
+              <Card className="mb-4">
+                <CardHeader className="pb-2">
+                  <CardTitle className="text-xl font-bold text-slate-800">
+                    Motorista Destaque do Mês
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-col items-center justify-between gap-4">
+                    <img
+                      src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      alt="Antônio Reis"
+                      className="h-28 w-28 rounded-full object-cover"
+                    />
+                    <div className="text-center">
+                      <p className="text-2xl font-bold">Antônio Reis</p>
+                      <div className="flex gap-2">
+                        <p className="text-md flex flex-col p-4">
+                          <span className="text-xl font-bold">220</span>{' '}
+                          Corridas
+                        </p>
+                        <p className="text-md flex flex-col border-x border-zinc-200 p-4">
+                          <span className="text-xl font-bold">R$ 4.840,00</span>
+                          Renda
+                        </p>
+                        <p className="text-md flex flex-col p-4">
+                          <span className="text-xl font-bold">4.8</span>{' '}
+                          Avaliação
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="pb-4">
+                  <CardTitle className="text-xl font-bold text-slate-900">
+                    Lista de Estabelecimentos Ativos
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {Array(6)
+                      .fill({})
+                      .map((_, index) => (
+                        <li key={index} className="mb-4 flex items-center">
+                          <div className="flex h-14 w-14 items-center justify-center rounded-full bg-blue-500 text-lg font-bold text-white">
+                            HP
+                          </div>
+                          <p className="ml-3 text-xl">Hambúrguer Pele</p>
+                        </li>
+                      ))}
+                  </ul>
+                </CardContent>
+              </Card>
             </div>
           </div>
         </div>
